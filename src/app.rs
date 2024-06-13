1281a0caac5845f780cb28153f82febeba68d530
Sebastian Gonzalez <devsebasgr@gmail.com>
2024-06-13 10:18:52 -0400

    More ui features

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,11 @@
-use egui::{FontFamily, FontId, Frame, Layout, Margin, RichText, TextStyle};
+use egui::{
+    ComboBox, FontFamily, FontId, Frame, Layout, Margin, RichText, SelectableLabel, Slider,
+    TextStyle,
+};
 use egui_extras::{Column, TableBuilder};
-use std::ops::RangeInclusive;
+use number_prefix::NumberPrefix;
+use rfd::AsyncFileDialog;
+use std::{collections::HashMap, ops::RangeInclusive};
 
 use crate::{
     bus::Bus,
@@ -8,32 +13,35 @@ use crate::{
     opcodes,
 };
 
-#[derive(serde::Deserialize, serde::Serialize)]
-#[serde(default)]
+#[derive(PartialEq, Eq)]
+pub enum RamRegion {
+    ZeroPage,
+    Stack,
+    All,
+}
+
+impl core::fmt::Display for RamRegion {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            RamRegion::ZeroPage => write!(f, "Zero page"),
+            RamRegion::Stack => write!(f, "Stack"),
+            RamRegion::All => write!(f, "All"),
+        }
+    }
+}
+
 pub struct Mos6502Emulator {
-    #[serde(skip)]
     bus: Bus,
-
-    #[serde(skip)]
     deassembly: Vec<(usize, String)>,
-
-    #[serde(skip)]
-    clock: std::time::Duration,
-
-    #[serde(skip)]
+    frecuency: f64,
     last_time: std::time::Instant,
-
-    #[serde(skip)]
     is_running: bool,
-
-    #[serde(skip)]
     mos6502: MOS6502,
-
-    #[serde(skip)]
-    ram_row: [u8; 54],
-
-    #[serde(skip)]
-    memory_editor: egui_memory_editor::MemoryEditor,
+    pause: bool,
+    instruction_index: usize,
+    breakpoint: HashMap<usize, bool>,
+    ram_region: RamRegion,
+    op_cycles: u8,
 }
 
 impl Default for Mos6502Emulator {
@@ -43,12 +51,15 @@ impl Default for Mos6502Emulator {
         Self {
             bus: Bus::new(Vec::new()),
             deassembly: Vec::new(),
-            clock: std::time::Duration::from_millis(200),
             last_time: std::time::Instant::now(),
             is_running: false,
             mos6502: MOS6502::new(),
-            ram_row: [' ' as u8; 54],
-            memory_editor,
+            pause: false,
+            instruction_index: 0,
+            breakpoint: HashMap::new(),
+            ram_region: RamRegion::All,
+            frecuency: 1.0,
+            op_cycles: 0,
         }
     }
 }
@@ -59,12 +70,11 @@ impl Mos6502Emulator {
             style.override_font_id = Some(FontId::new(18.0, FontFamily::Proportional));
         });
 
-        if let Some(storage) = cc.storage {
-            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
-        }
+        // if let Some(storage) = cc.storage {
+        //     return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
+        // }
 
-        let a = Self::default();
-        a
+        Self::default()
     }
 }
 
@@ -73,9 +83,15 @@ impl eframe::App for Mos6502Emulator {
         if self.is_running {
             ctx.request_repaint();
             let now = std::time::Instant::now();
-            if now.duration_since(self.last_time) >= self.clock {
-                self.last_time = now;
-                self.mos6502.clock(&mut self.bus);
+            if now.duration_since(self.last_time).as_secs_f64() >= 1.0 / self.frecuency {
+                if !self.pause {
+                    self.last_time = now;
+                    if let Some(op) = self.mos6502.clock(&mut self.bus) {
+                        self.instruction_index =
+                            usize::clamp(self.instruction_index + 1, 0, self.deassembly.len() - 1);
+                        self.op_cycles = op.cycles;
+                    }
+                }
             }
         }
 
@@ -84,12 +100,35 @@ impl eframe::App for Mos6502Emulator {
             .show(ctx, |ui| {
                 ui.with_layout(Layout::left_to_right(egui::Align::TOP), |ui| {
                     if ui.button(RichText::new("Load ROM").size(16.0)).clicked() {
+                        let files = AsyncFileDialog::new()
+                            .add_filter("prg", &["PRG", "prg"])
+                            .pick_file();
+                        if let Some(file_handle) = async_std::task::block_on(async { files.await })
+                        {
+                            self.bus.rom =
+                                async_std::task::block_on(async { file_handle.read().await });
+                            self.bus.ram.fill(0);
+
+                            self.deassembly = opcodes::deassembly(&mut self.bus);
+
+                            self.last_time = std::time::Instant::now();
+                            self.is_running = true;
+
+                            self.mos6502.reset(&mut self.bus);
+                            self.breakpoint.clear();
+                            self.instruction_index = 0;
+                            self.op_cycles = self.mos6502.cycles;
+                        }
+                    }
+
+                    if ui.button(RichText::new("Load sample").size(16.0)).clicked() {
                         let src = [
                             0xa2, 0x00, 0xa0, 0x00, 0x8a, 0x99, 0x00, 0x02, 0x48, 0xe8, 0xc8, 0xc0,
                             0x10, 0xd0, 0xf5, 0x68, 0x99, 0x00, 0x02, 0xc8, 0xc0, 0x20, 0xd0, 0xf7,
                         ];
 
                         self.bus.rom = src.to_vec();
+                        self.bus.ram.fill(0);
 
                         self.deassembly = opcodes::deassembly(&mut self.bus);
 
@@ -97,27 +136,67 @@ impl eframe::App for Mos6502Emulator {
                         self.is_running = true;
 
                         self.mos6502.reset(&mut self.bus);
+                        self.breakpoint.clear();
+                        self.instruction_index = 0;
+                        self.op_cycles = self.mos6502.cycles;
                     }
                     ui.add_space(16.0);
 
-                    if ui.button("▶").clicked() {}
+                    if ui.button("▶").clicked() {
+                        self.pause = false;
+                    }
 
-                    if ui.button("⏸").clicked() {}
+                    let mut pause_btn = ui.button("⏸");
+                    if self.pause {
+                        pause_btn = pause_btn.highlight();
+                    }
+                    if pause_btn.clicked() {
+                        self.pause = !self.pause;
+                    }
 
-                    if ui.button("↺").clicked() {}
+                    if ui.button("↺").clicked() {
+                        self.mos6502.reset(&mut self.bus);
+                    }
 
                     ui.add_space(16.0);
 
-                    // Up
-                    if ui.button("⏶").clicked() {}
+                    // stack back
+                    if ui.button("⏶").clicked() {
+                        self.instruction_index = self.instruction_index.saturating_sub(1);
 
-                    // Down
-                    if ui.button("⏷").clicked() {}
+                        self.mos6502.pc =
+                            self.deassembly.get(self.instruction_index).unwrap().0 as u16;
+                    }
+
+                    // step up
+                    if ui.button("⏷").clicked() {
+                        if self.instruction_index < self.deassembly.len() - 1 {
+                            self.instruction_index =
+                                usize::min(self.instruction_index + 1, self.deassembly.len() - 1);
+
+                            self.mos6502.pc =
+                                self.deassembly.get(self.instruction_index).unwrap().0 as u16;
+                        }
+                    }
+
+                    ui.add_space(16.0);
 
                     if self.is_running {
+                        let result = match NumberPrefix::decimal(self.frecuency) {
+                            NumberPrefix::Standalone(v) => {
+                                format!("Clock: {:.1} Hz", v)
+                            }
+                            NumberPrefix::Prefixed(prefix, v) => {
+                                format!("Clock: {:.1} {}HZ", v, prefix)
+                            }
+                        };
+                        ui.add(Slider::new(&mut self.frecuency, 1.0..=1000.0).step_by(0.1));
+                        ui.label(result);
+
+                        ui.add_space(16.0);
                         ui.label(format!(
-                            "Clock: {} Hz",
-                            1f64 / self.clock.as_millis() as f64
+                            "Cycles {} / {}",
+                            self.mos6502.cycles, self.op_cycles
                         ));
                     }
                 })
@@ -136,27 +215,27 @@ impl eframe::App for Mos6502Emulator {
                         ui.add_space(18.0);
 
                         ui.add(
-                            egui::Slider::new(&mut self.mos6502.pc, 0x0..=0xFFFF)
+                            Slider::new(&mut self.mos6502.pc, 0x0..=0xFFFF)
                                 .custom_formatter(format_slider_hex(6))
                                 .prefix("PC: "),
                         );
                         ui.add(
-                            egui::Slider::new(&mut self.mos6502.ac, 0x0..=0xFF)
+                            Slider::new(&mut self.mos6502.ac, 0x0..=0xFF)
                                 .custom_formatter(format_slider_hex(4))
                                 .prefix("A: "),
                         );
                         ui.add(
-                            egui::Slider::new(&mut self.mos6502.x, 0x0..=0xFF)
+                            Slider::new(&mut self.mos6502.x, 0x0..=0xFF)
                                 .custom_formatter(format_slider_hex(4))
                                 .prefix("X: "),
                         );
                         ui.add(
-                            egui::Slider::new(&mut self.mos6502.y, 0x0..=0xFF)
+                            Slider::new(&mut self.mos6502.y, 0x0..=0xFF)
                                 .custom_formatter(format_slider_hex(4))
                                 .prefix("Y: "),
                         );
                         ui.add(
-                            egui::Slider::new(&mut self.mos6502.stack_index, 0x0..=0xFF)
+                            Slider::new(&mut self.mos6502.stack_index, 0x0..=0xFF)
                                 .custom_formatter(format_slider_hex(4))
                                 .prefix("SP: "),
                         );
@@ -165,7 +244,7 @@ impl eframe::App for Mos6502Emulator {
 
                         ui.horizontal(|ui| {
                             if ui
-                                .add(egui::SelectableLabel::new(
+                                .add(SelectableLabel::new(
                                     self.mos6502.get_flag(Flags::N) != 0,
                                     "N",
                                 ))
@@ -174,27 +253,27 @@ impl eframe::App for Mos6502Emulator {
                                 self.mos6502
                                     .set_flag(Flags::N, !(self.mos6502.get_flag(Flags::N) != 0));
                             }
-                            ui.add(egui::SelectableLabel::new(
+                            ui.add(SelectableLabel::new(
                                 self.mos6502.get_flag(Flags::V) != 0,
                                 "V",
                             ));
-                            ui.add(egui::SelectableLabel::new(
+                            ui.add(SelectableLabel::new(
                                 self.mos6502.get_flag(Flags::B) != 0,
                                 "B",
                             ));
-                            ui.add(egui::SelectableLabel::new(
+                            ui.add(SelectableLabel::new(
                                 self.mos6502.get_flag(Flags::D) != 0,
                                 "D",
                             ));
-                            ui.add(egui::SelectableLabel::new(
+                            ui.add(SelectableLabel::new(
                                 self.mos6502.get_flag(Flags::I) != 0,
                                 "I",
                             ));
-                            ui.add(egui::SelectableLabel::new(
+                            ui.add(SelectableLabel::new(
                                 self.mos6502.get_flag(Flags::Z) != 0,
                                 "Z",
                             ));
-                            ui.add(egui::SelectableLabel::new(
+                            ui.add(SelectableLabel::new(
                                 self.mos6502.get_flag(Flags::C) != 0,
                                 "C",
                             ));
@@ -224,11 +303,11 @@ impl eframe::App for Mos6502Emulator {
                                             row.set_selected(true);
                                         }
                                         row.col(|ui| {
-                                            ui.label(format!("{:04X}", STACK_TOP + 0xFF - i));
+                                            ui.label(format!("{:03X}", STACK_TOP + 0xFF - i));
                                         });
                                         row.col(|ui| {
                                             ui.label(format!(
-                                                "{:#04X}",
+                                                "{:02X}",
                                                 self.bus.ram[(STACK_TOP + 0xFF - i) as usize]
                                             ));
                                         });
@@ -244,21 +323,73 @@ impl eframe::App for Mos6502Emulator {
             .show(ctx, |ui| {
                 ui.vertical_centered(|ui| {
                     ui.label(RichText::new("RAM").heading().strong().size(26.0));
-                    ui.add_space(18.0);
-
-                    if self.is_running {
-                        self.memory_editor.draw_editor_contents_read_only(
-                            ui,
-                            &mut self.bus,
-                            |bus, address| Some(bus.ram[address]),
-                        );
-                    }
+                    ComboBox::from_label("Select Region")
+                        .selected_text(self.ram_region.to_string())
+                        .show_ui(ui, |ui| {
+                            ui.selectable_value(
+                                &mut self.ram_region,
+                                RamRegion::ZeroPage,
+                                "Zero page",
+                            );
+                            ui.selectable_value(&mut self.ram_region, RamRegion::Stack, "Stack");
+                            ui.selectable_value(&mut self.ram_region, RamRegion::All, "All");
+                        });
+                    ui.add_space(10.0);
                 });
+                let text_style = TextStyle::Body;
+                let height = ui.text_style_height(&text_style) + 5.0;
+                let (ram_slice, offset): (&[u8], usize) = match self.ram_region {
+                    RamRegion::ZeroPage => (&self.bus.ram[0..=0xFF], 0),
+                    RamRegion::Stack => (
+                        &self.bus.ram[STACK_TOP as usize..=STACK_TOP as usize + 0xFF],
+                        STACK_TOP as usize,
+                    ),
+                    RamRegion::All => (self.bus.ram.as_slice(), 0),
+                };
+
+                TableBuilder::new(ui)
+                    .striped(true)
+                    .resizable(true)
+                    .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
+                    .column(Column::auto())
+                    .column(Column::remainder().resizable(false))
+                    .header(20.0, |mut header| {
+                        header.col(|ui| {
+                            ui.strong("Address");
+                        });
+
+                        header.col(|ui| {
+                            ui.strong("Values");
+                        });
+                    })
+                    .body(|body| {
+                        body.rows(height, ram_slice.len() / 16, |mut row| {
+                            let row_index = row.index();
+                            row.col(|ui| {
+                                ui.label(format!("{:04X}", (row_index * 16) + offset));
+                            });
+                            row.col(|ui| {
+                                let mut asd: [u8; 48] = [0u8; 48];
+                                let mut offset = 0;
+                                for i in 0..16 {
+                                    let val_str =
+                                        format!("{:02X}", ram_slice[(row_index * 16) + i]);
+                                    asd[offset] = val_str.chars().nth(0).unwrap() as u8;
+                                    asd[offset + 1] = val_str.chars().nth(1).unwrap() as u8;
+                                    asd[offset + 2] = ' ' as u8;
+                                    offset += 3;
+                                }
+                                ui.label(std::str::from_utf8(&asd).unwrap());
+                            });
+                        })
+                    })
             });
 
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.label(RichText::new("Disassembly").heading().strong().size(26.0));
             ui.add_space(18.0);
+            let text_style = TextStyle::Body;
+            let height = ui.text_style_height(&text_style) + 10.0;
 
             TableBuilder::new(ui)
                 .striped(true)
@@ -277,21 +408,32 @@ impl eframe::App for Mos6502Emulator {
                         ui.strong("Instruction");
                     });
                 })
-                .body(|mut body| {
-                    for (address, instruction) in self.deassembly.iter() {
-                        body.row(20.0, |mut row| {
-                            if self.mos6502.pc as usize == *address {
-                                row.set_selected(true);
+                .body(|body| {
+                    body.rows(height, self.deassembly.len(), |mut row| {
+                        let row_index = row.index();
+                        let row_data = self.deassembly.get(row_index).unwrap();
+                        let breakpoint_value = self.breakpoint.get(&row_index).cloned();
+                        if self.mos6502.pc as usize == row_data.0 {
+                            if breakpoint_value.unwrap_or(false) {
+                                self.pause = true
                             }
-                            row.col(|ui| if ui.radio(false, "").clicked() {});
-                            row.col(|ui| {
-                                ui.label(format!("{:06X}", address));
-                            });
-                            row.col(|ui| {
-                                ui.label(instruction);
-                            });
+                            row.set_selected(true);
+                        }
+                        row.col(|ui| {
+                            if ui.radio(breakpoint_value.unwrap_or(false), "").clicked() {
+                                match breakpoint_value {
+                                    Some(v) => self.breakpoint.insert(row_index, !(v)),
+                                    None => self.breakpoint.insert(row_index, true),
+                                };
+                            }
                         });
-                    }
+                        row.col(|ui| {
+                            ui.label(format!("{:04X}", row_data.0));
+                        });
+                        row.col(|ui| {
+                            ui.label(&row_data.1);
+                        });
+                    });
                 });
         });
     }
