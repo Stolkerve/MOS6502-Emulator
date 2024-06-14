b1877ec351c49789c5fefbed25e88a6009dbddd9
Stolkerve <devsebasgr@gmail.com>
2024-06-14 03:08:35 +0000

    Deploying to gh-pages from @ Stolkerve/MOS6502-Emulator@a4cd5d992bec23b6632abcc78c8e31567aa205e8 🚀

---
+++ sw.js
@@ -0,0 +1,25 @@
+var cacheName = 'mos6502-emulator-pwa';
+var filesToCache = [
+  './',
+  './index.html',
+  './mos6502_emulator.js',
+  './mos6502_emulator_bg.wasm',
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
